e35fb0babd5a74d1974eb2d80000cdeba28b813d
esplusnin <esplusnin94@yandex.ru>
2026-03-04 10:36:54 +0300

    Initial Commit

---
+++ fitness_coach/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fitness_coach
+//
+//  Created by Евгений on 04.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fitness_coach/fitness_coachApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitness_coachApp.swift
+//  fitness_coach
+//
+//  Created by Евгений on 04.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct fitness_coachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
